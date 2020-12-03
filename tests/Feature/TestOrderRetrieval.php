1edde364b792521a3ec0904c9d53cfcfd37cb98f
Vin Aquino <melvinaquino@ninepinetech.com>
2020-12-03 22:46:01 +0800

    Added orders tests

---
+++ tests/Feature/TestOrderRetrieval.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\{WithoutMiddleware,WithFaker};
+use Tests\TestCase;
+
+class TestOrderRetrieval extends AdminAccountTest
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testOrdersListWithToken()
+    {
+        $this->initialUser();
+        $response = $this->withHeaders([
+            'X-Requested-With' => 'XMLHttpRequest',
+            'Authorization'    => 'Bearer ' . $this->loginJsonResponse->token
+            ])->json('GET', '/api/orders', 
+                [
+                    'id'   => $this->faker->randomDigit
+                ]
+            );
+        $response->assertStatus(200);
+    }
+
+    public function testOrdersListListWithoutToken() {
+        $response = $this->withHeaders([
+            'X-Requested-With' => 'XMLHttpRequest',
+            'Authorization'    => 'Bearer '
+            ])->json('GET', '/api/orders', 
+                [
+                    'id'   => $this->faker->randomDigit,
+                ]
+            );
+
+        $response->assertJson(['message' => 'Unauthenticated.']);
+    }
+}
