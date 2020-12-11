e98ae8ce80d955ecc4e7ca01b0ac49645b44c090
Vin Aquino <melvinaquino@ninepinetech.com>
2020-12-11 16:04:26 +0800

    initial customer files

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\{UserWallet, Currency};
+use Illuminate\Support\Arr;
+
+class User extends Model
+{
+    //
+}
