0af57128897d2e90ebdb00bda48b0f3d693e9748
Gio Rodriguez <giorodriguez@ninepinetech.com>
2021-01-24 14:26:45 +0800

    Admin activity logs for models

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -4,10 +4,12 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
 
 class AdminUser extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, LogsActivity;
     protected $table = 'admin_users';
     /**
      * The attributes that are mass assignable.
@@ -25,6 +27,23 @@ class AdminUser extends Authenticatable
       'password',
       'remember_token'
     ];
+
+    protected static $logAttributes = ['name', 'email', 'status'];
+
+    protected static $logOnlyDirty = true;
+
+    protected static $logName = 'Admin Users';
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+      $activity->properties = $activity->properties->put('action', ucfirst($eventName));
+      $activity->properties = $activity->properties->put('ip_address', request()->ip());
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return ucfirst($eventName)." admin user: ".request()->email;
+    }
 
     public static function getAll()
     {
--- app/Models/ProviderAccount.php
+++ app/Models/ProviderAccount.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
 
 class ProviderAccount extends Model
 {
     protected $table = "provider_accounts";
 
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
 
     protected $fillable = [
         'username',
@@ -26,6 +28,23 @@ class ProviderAccount extends Model
         'created_at',
         'updated_at',
     ];
+
+    protected static $logAttributes = ['username', 'type', 'punter_percentage', 'provider_id', 'is_enabled', 'is_idle'];
+
+    protected static $logOnlyDirty = true;
+
+    protected static $logName = 'Provider Accounts';
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+      $activity->properties = $activity->properties->put('action', ucfirst($eventName));
+      $activity->properties = $activity->properties->put('ip_address', request()->ip());
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return ucfirst($eventName)." provider account: ".request()->username;
+    }
 
     public function providers()
     {
