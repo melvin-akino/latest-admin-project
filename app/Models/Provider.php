b7c70105402fa94e65813e336da5b0dd253abec9
Melvin Aquino <melvinaquino@ninepinetech.com>
2021-02-23 12:11:13 +0800

    Added spatie admin logger

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
 
 class Provider extends Model
 {
     protected $table = 'providers';
+    use LogsActivity;
 
     protected $fillable = [
         'name',
@@ -20,4 +23,21 @@ class Provider extends Model
         'created_at',
         'updated_at',
     ];
+
+    protected static $logAttributes = ['name', 'alias', 'punter_percentage', 'is_enabled', 'currency_id'];
+
+    protected static $logOnlyDirty = true;
+
+    protected static $logName = 'Providers';
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+      $activity->properties = $activity->properties->put('action', ucfirst($eventName));
+      $activity->properties = $activity->properties->put('ip_address', request()->ip());
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return ucfirst($eventName)." providers: ".request()->name;
+    }
 }
