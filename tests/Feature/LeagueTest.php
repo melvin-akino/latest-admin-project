b2903185a7c2948bffec09e191808368f5d5aaab
Klane Zurbano <klanezurbano@ninepinetech.com>
2021-03-08 10:53:18 +0800

    :white_check_mark: Add tests for get leagues/teams by provider id API

---
+++ tests/Feature/LeagueTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\{WithoutMiddleware,WithFaker};
+use Tests\TestCase;
+
+class LeagueTest extends AdminAccountTestCase
+{
+    public function testLeaguesByProviderIdWithToken()
+    {
+        $this->initialUser();
+        $response = $this->get('/api/leagues/1', [
+            'X-Requested-With' => 'XMLHttpRequest',
+            'Authorization'    => 'Bearer ' . $this->loginJsonResponse->token
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            '*' => [
+                'name',
+                'sport_id',
+                'provider_id'
+            ]
+        ]);
+
+    }
+
+    public function testLeaguesByProviderIdWithoutToken() {
+        $response = $this->get('/api/leagues/1', [
+            'X-Requested-With' => 'XMLHttpRequest',
+            'Authorization'    => 'Bearer XXX'
+        ]);
+
+        $response->assertJson(['message' => 'Unauthenticated.']);
+    }
+
+}
