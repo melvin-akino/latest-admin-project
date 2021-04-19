7434bf4333b68470fd0c9029f7244f7bdb93877b
Melvin Aquino <melvinaquino@ninepinetech.com>
2021-04-19 22:53:54 +0800

    test simulator for leagues and teams

--- test-pgsql-simulator/test-team-simulator.php
+++ test-pgsql-simulator/test-team-simulator.php
@@ -4,30 +4,30 @@ $val = $_SERVER['argv'];
 
 $sportId = $val[1];
 $providerId = $val[2];
-$leagueCount = $val[3];
+$teamCount = $val[3];
 $randomFormatching = !empty($val[4]) ? $val[4] : 0;
 
 $db = pg_connect("host=172.20.0.2 port=5432 dbname=multiline_coro user=postgres password=password");
 //$result = pg_query($db, "SELECT * FROM book where book_id = '$_POST[bookid]'");
 //$row = pg_fetch_assoc($result);
-$totalRecordsToInsert = $leagueCount - $randomFormatching;
+$totalRecordsToInsert = $teamCount - $randomFormatching;
 for ($i=0; $i<$totalRecordsToInsert; $i++)
 {
-    $leagueName = getName(random_int(16, 155));
+    $teamName = getName(random_int(16, 32));
     
-    $result = pg_query($db, "insert into leagues (name, provider_id, sport_id) values ('$leagueName', $providerId, $sportId)");
+    $result = pg_query($db, "insert into teams (name, provider_id, sport_id) values ('$teamName', $providerId, $sportId)");
     if ($result) {
-        echo "League Name: $leagueName has been added into leagues table.\n";
+        echo "Team Name: $teamName has been added into teams table.\n";
     }
 }
 
 if (!empty($randomFormatching)) {
-    $result = pg_query($db, "SELECT name FROM leagues order by random() limit $randomFormatching");
+    $result = pg_query($db, "SELECT name FROM teams order by random() limit $randomFormatching");
     while($row = pg_fetch_assoc($result))
     {
-        $result = pg_query($db, "insert into leagues (name, provider_id, sport_id) values ('{$row['name']}', $providerId, $sportId)");
+        $result = pg_query($db, "insert into teams (name, provider_id, sport_id) values ('{$row['name']}', $providerId, $sportId)");
         if ($result) {
-            echo "League Name: $leagueName has been added into leagues table.\n";
+            echo "Team Name: $teamName has been added into teams table.\n";
         }    
     }
 }
