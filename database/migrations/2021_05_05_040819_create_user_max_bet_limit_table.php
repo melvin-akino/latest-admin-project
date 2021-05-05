798868fa49fbcd8569637e9bd5b7a19afd3c911a
Gio Rodriguez <giorodriguez@ninepinetech.com>
2021-05-05 12:57:40 +0800

    Created user_max_bet_limit table

---
+++ database/migrations/2021_05_05_040819_create_user_max_bet_limit_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserMaxBetLimitTable extends Migration
+{
+    protected $tablename = "user_max_bet_limit";
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablename, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->index();
+            $table->double('max_bet_limit')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+
+        Artisan::call('db:seed', [
+            '--class' => UserMaxBetLimitSeeder::class
+        ]);
+      }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
+}
---
+++ database/seeds/UserMaxBetLimitSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\{ User, SystemConfiguration, UserMaxBetLimit };
+
+class UserMaxBetLimitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+
+        foreach($users as $user) {
+            UserMaxBetLimit::updateOrCreate(
+              [ 'user_id' => $user->id ], 
+              [ 'max_bet_limit' => SystemConfiguration::getValueByType('MAX_BET') ]
+            );
+        }
+    }
+}
