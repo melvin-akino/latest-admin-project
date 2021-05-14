cf7934db21e635b46b6123866d0d9b14f3d4652a
Kevin Uy <kevinuy@ninepinetech.com>
2021-05-14 14:17:30 +0800

    Updated SQL View for 'to_win' and 'profit_loss'

--- database/migrations/2021_05_13_220141_create_view_my_orders_v2.php
+++ database/migrations/2021_05_13_220141_create_view_my_orders_v2.php
@@ -25,8 +25,8 @@ class CreateViewMyOrdersV2 extends Migration
                             ot.type
                     END
                 ) AS odd_type,
-                ub.odds * ub.stake AS to_win,
-                (SELECT SUM(pb.stake) FROM provider_bets AS pb WHERE pb.user_bet_id = ub.id AND pb.settled_date IS NOT NULL) * ub.odds AS profit_loss
+                (SELECT SUM(pb.to_win) FROM provider_bets AS pb WHERE pb.user_bet_id = ub.id) AS to_win,
+                (SELECT SUM(pb.profit_loss) FROM provider_bets AS pb WHERE pb.user_bet_id = ub.id) AS profit_loss
             FROM user_bets AS ub
             JOIN odd_types AS ot
                 ON ot.id = ub.odd_type_id
